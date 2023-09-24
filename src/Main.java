8f91f861e32f3bd19be6364ace1c583f3becd13c
dongi <dongibson633@gmail.com>
2023-09-23 22:30:50 -0400

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,22 @@ public class Main
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        double totalCost = 0.0;
+        double totalTax = 0.0;
+
+        System.out.print("What is the cost of your product? ");
+
+        totalCost = in.nextInt();
+        in.nextLine();
+        if(totalCost >= 100)
+        {
+            System.out.println("Your shipping is free! Your cost is " + totalCost);
+        }
+        else
+        {
+            totalTax = totalCost * 1.02;
+            System.out.println("Your total cost with tax is " + totalTax);
+        }
+
     }
 }
